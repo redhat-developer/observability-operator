a3cd069fe4c0b05180d0d8aeb8a90d75a7f4142f
Jeremy Ary <jeremy.ary@gmail.com>
2021-01-26 16:26:35 -0600

    create resources after fetch

--- controllers/reconcilers/configuration/configuration_reconciler.go
+++ controllers/reconcilers/configuration/configuration_reconciler.go
@@ -5,7 +5,10 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	v13 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	"github.com/ghodss/yaml"
 	"github.com/go-logr/logr"
+	"github.com/integr8ly/grafana-operator/v3/pkg/apis/integreatly/v1alpha1"
 	v1 "github.com/jeremyary/observability-operator/api/v1"
 	"github.com/jeremyary/observability-operator/controllers/reconcilers"
 	"io/ioutil"
@@ -14,6 +17,7 @@ import (
 	"net/http"
 	"net/url"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 const (
@@ -32,8 +36,13 @@ type GrafanaIndex struct {
 	Dashboards []string `json:"dashboards"`
 }
 
+type PrometheusIndex struct {
+	Rules []string `json:"rules"`
+}
+
 type RepositoryConfig struct {
 	Grafana *GrafanaIndex `json:"grafana"`
+	Prometheus *PrometheusIndex `json:"prometheus"`
 }
 
 type RepositoryIndex struct {
@@ -96,7 +105,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, cr *v1.Observability, s *v1.
 	// Collect index files
 	var indexes []RepositoryIndex
 	for _, repoInfo := range repos {
-		indexBytes, err := r.readIndexFile(&repoInfo)
+		indexBytes, err := r.readResourceFile(&repoInfo, "index.json")
 		if err != nil {
 			r.logger.Error(err, "error reading repository index")
 			continue
@@ -110,20 +119,64 @@ func (r *Reconciler) Reconcile(ctx context.Context, cr *v1.Observability, s *v1.
 		}
 
 		indexes = append(indexes, index)
+
+		for _, index := range indexes {
+			if err := r.createGrafanaDashboards(cr.Namespace, &repoInfo, index.Config.Grafana.Dashboards); err != nil {
+				r.logger.Error(err, "error creating Grafana dashboards")
+				continue
+			}
+			if err := r.createPrometheusRules(cr.Namespace, &repoInfo, index.Config.Prometheus.Rules); err != nil {
+				r.logger.Error(err, "error create Prometheus rules")
+				continue
+			}
+		}
 	}
 
-	// Create dashboards
-	// TODO
-
 	return v1.ResultSuccess, nil
+}
+
+func (r *Reconciler) createGrafanaDashboards(namespace string, repo *RepositoryInfo, dashboardPaths []string) error {
+	for _, path := range dashboardPaths {
+		var dashboard v1alpha1.GrafanaDashboard
+		dashboardBytes, err := r.readResourceFile(repo, path)
+		if err != nil {
+			r.logger.Error(err, "failed to create Grafana dashboard", "path", path)
+		}
+		if err := yaml.Unmarshal(dashboardBytes, &dashboard); err != nil {
+			r.logger.Error(err, "failed to parse dashboard YAML file", "path", path)
+		}
+		dashboard.Namespace = namespace
+		_, err = controllerutil.CreateOrUpdate(context.Background(), r.client, &dashboard, func() error {
+			return nil
+		})
+	}
+	return nil
+}
+
+func (r *Reconciler) createPrometheusRules(namespace string, repo *RepositoryInfo, rulePaths []string) error {
+	for _, path := range rulePaths {
+		var rule v13.PrometheusRule
+		ruleBytes, err := r.readResourceFile(repo, path)
+		if err != nil {
+			r.logger.Error(err, "failed to create Prometheus rule", "path", path)
+		}
+		if err := yaml.Unmarshal(ruleBytes, &rule); err != nil {
+			r.logger.Error(err, "failed to parse rule YAML file", "path", path)
+		}
+		rule.Namespace = namespace
+		_, err = controllerutil.CreateOrUpdate(context.Background(), r.client, &rule, func() error {
+			return nil
+		})
+	}
+	return nil
 }
 
 func (r *Reconciler) Cleanup(ctx context.Context, cr *v1.Observability) (v1.ObservabilityStageStatus, error) {
 	return v1.ResultSuccess, nil
 }
 
-func (r *Reconciler) readIndexFile(repo *RepositoryInfo) ([]byte, error) {
-	repoUrl := fmt.Sprintf("%s/%s/index.json", repo.Repository, repo.Channel)
+func (r *Reconciler) readResourceFile(repo *RepositoryInfo, path string) ([]byte, error) {
+	repoUrl := fmt.Sprintf("%s/%s/%s", repo.Repository, repo.Channel, path)
 
 	req, err := http.NewRequest(http.MethodGet, repoUrl, nil)
 	if err != nil {
@@ -131,6 +184,7 @@ func (r *Reconciler) readIndexFile(repo *RepositoryInfo) ([]byte, error) {
 	}
 
 	req.Header.Set("Authorization", fmt.Sprintf("token %s", repo.AccessToken))
+	req.Header.Set("Accept", "application/vnd.github.v3.raw")
 
 	resp, err := r.httpClient.Do(req)
 	if err != nil {
