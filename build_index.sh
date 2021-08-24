c6241b0017519063e3f69fc4f5007ad3d0e6f7d1
Peter Braun <pbraun@redhat.com>
2021-08-24 22:49:45 +0200

    fix: do not rely on opm to run in ci

--- build_index.sh
+++ build_index.sh
@@ -17,10 +17,7 @@
 
 # This script builds and deploys the Observability Operator Index using the OPM CLI.
 
-# Print GLIBC version
-ldd --version
-
-OPM_VERSION=v1.17.0
+OPM_VERSION=v1.17.5
 OPM_PLATFORM=linux-amd64
 OPM_DOWNLOAD_URL="https://github.com/operator-framework/operator-registry/releases/download/$OPM_VERSION/$OPM_PLATFORM-opm"
 
